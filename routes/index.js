f09ff043e363875d9c11ddcdb164cf517a8380d0
Ahmad <alsaleh.a.ahmad@gmail.com>
2018-11-16 21:18:24 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', require('../public/index/translation/english'));
+  res.render('index', require('../public/index/translation/arabic'));
+});
+
+router.get('/perfum', (req, res, next) => {
+  res.render('perfum');
 });
 
 module.exports = router;
