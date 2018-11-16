1801645d5279a76fde9c788ac268832aef97a542
Ahmad <alsaleh.a.ahmad@gmail.com>
2018-11-16 20:53:46 +0200

    first commit

---
+++ public/index/translation/english.js
@@ -0,0 +1,47 @@
+module.exports = {
+    "title" : "Alshami Group",
+    "subtitle" : "برؤية دمشقية أصيلة",
+    "more" : "More Info",
+    "contactUs" : {
+        "title" : "Contact Us",
+        "subtitle" : "Contact with Group Manager",
+        "address" : "Syria, Damascus, Sarouja",
+        "phone" : "+963 933286077",
+        "email" : "admin@alshamigroup.com"
+    },
+    "intro" : {
+        "title": "What do Alshami offer?",
+        "text1" : "الشامي هي مجموعة من الشركات التي انطلقت من الأحياء الدمشقية وانتشرت في المدن السورية",
+        "text2": "يمثل العطر الشامي العصب الرئيسي .. باﻹضافة إلى مطاعم الديك الشامي للوجبات السريعة ومحلات العسل الشامي",
+        "text3"  :"تأسست مجموعة الشامي على يد السيد عمر طالب إنطلاقاً من محل العطر الشامي في منطقة ساروجة ثم امتدت لتصل إلى الصدارة في غضون 15 سنة"
+    }, 
+    "perfum" : {
+        "title" : "Alshami Perfume",
+        "description" : "Your Gate To The Luxury" + "\n"
+            + "Of the Old Damascene Neighborhoods" + "\n"
+            + "and Purity Jasmine" + "\n"
+            + "Quality, confidence, proliferation" + "\n"
+            + "We offer the finest perfumes" + "\n"
+            + "from Damascus to the Syrian coast" + "\n"
+            + "and then the Turkish cities" + "\n"
+            + "International categories of" + "\n" 
+            + "French MANE factories" + "\n"
+            + "Try the new global varieties" + "\n"
+            + "For Men & Women" 
+    },
+    "honey" : {
+        "title" : "العسل الشامي",
+        "description" : "حيث تجد أجود أنواع العسل الطبيعي" + "\n"
+        + "100% من إنتاج المزارع السورية" + "\n"
+        + "باﻹضافة لمنتجات وخلطات عديدة من العسل مع المكسرات" + "\n" 
+        + "انطلقنا من جوار العطر الشامي في ساروجة ونتطلع للمزيد"
+    },
+    "locoq" : {
+        "title" : "الديك الشامي Le COQ",
+        "description" : "الديك الشامي مطعم الوحبات السريعة المفضل في الساحل السوري حيث تجد النكهة اللذيذة للوجبات الغربية من لمسة من الوجبات العربية والخليجية"
+            + "نفتخر بتقديم أشهى الوجبات والسندويش في مدينة اللاذقية"
+    },
+    "footer" : {
+
+    }
+}
